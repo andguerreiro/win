40998cf09ecb6eba9c116fe8f596ea4f3ebfa339
andguerreiro <andguerreiro@yahoo.com>
2026-08-29 13:09:49 -0300

    Update clock.bat

--- clock.bat
+++ clock.bat
@@ -1,5 +1,13 @@
 @echo off
+
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
 powershell -Command "Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation' -Name 'RealTimeIsUniversal' -Type DWord -Value 1"
+
 echo.
-echo Done!
+echo Done.
 pause

b1e80fdeb59baa49eecb9ab59443c107beef9694
andguerreiro <andguerreiro@yahoo.com>
2026-08-29 13:12:27 -0300

    Update clock.ps1

--- clock.ps1
+++ clock.ps1
@@ -1,1 +1,5 @@
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation" -Name "RealTimeIsUniversal" -Type DWord -Value 1
+
+Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation" -Name "RealTimeIsUniversal"
+
+Read-Host "Type Enter to exit"
